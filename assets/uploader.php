91e6ce9891f57c2c98f4cafd172e6fe14b36eea2
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-04-07 19:11:14 -0500

    Add Back-end logic to upload each file to the files folder

---
+++ assets/uploader.php
@@ -0,0 +1,27 @@
+<?php
+// var_dump($_FILES["file"]);
+
+if (isset($_FILES["file"])) {
+  $name = $_FILES["file"]["name"];
+  $file = $_FILES["file"]["tmp_name"];
+  $error = $_FILES["file"]["error"];
+  $destination = "../files/$name";
+  $fileIsUploaded = move_uploaded_file($file, $destination);
+
+  if ($fileIsUploaded) {
+    $res = array(
+      "err" => false,
+      "status" => http_response_code(200),
+      "statusText" => "Archivo $name subido con éxito",
+      "files" => $_FILES["file"]
+    );
+  } else {
+    $res = array(
+      "err" => true,
+      "status" => http_response_code(400),
+      "statusText" => "Error al subir el archivo $name",
+      "files" => $_FILES["file"]
+    );
+  }
+  echo json_encode($res);
+}
